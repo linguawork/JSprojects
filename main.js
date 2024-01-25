07a0a51b1ddfd5a4ab4af582db8c251b67c4ab59
Arnette Reggie <mixedworldcitizen@gmail.com>
2024-01-25 11:36:45 +0100

    24 01 25, API vars chosen, link and params hooked, printed to console

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import './style.css'
+import { getWeather } from './weather'
+
+
+/**
+ Intl.DateTimeFormat().resolvedOptions().timeZone 
+
+ This is how we get time zone, it will give us our current location time
+ */
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
+.then(res => {
+    console.log(res.data)
+})
--- weather.js
+++ weather.js
@@ -18,9 +18,40 @@ daily vars:
     min appar temp (2m),
     precip sum
 
-using current weather settings
+using current weather settings:
+    temp, 
+    apparent temp, 
+    weather code,
+    wind speed(10m)
+
+settings: 
+Celsius
+Km/h
+Mm
+unix timestamp 
+
+Api response: 
+    chart and URL(open in a new tab, cpy url)
+
+    https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,apparent_temperature,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&timeformat=unixtime&timezone=Europe%2FBerlin
+
+    https://api.open-meteo.com/v1/forecast? current=temperature_2m,apparent_temperature,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&timeformat=unixtime
+    (this the shortened link, since long and latit, timezone are passed separately, they are erased from the link)
+
+https://api.open-meteo.com/v1/forecast is a link before the query
+Use axios: 
+npm i axios
 */
 
-export function getWeather(latitude, longitude, timezone){
+import axios from "axios";
 
+
+export function getWeather(latit, longit, timezone){
+    //the shortened link
+    //the params may be passed directly or from the link. Anyway will work
+   return axios.get("https://api.open-meteo.com/v1/forecast?current=temperature_2m,apparent_temperature,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&timeformat=unixtime", { params:{
+        latitude : latit, 
+        longitude: longit, 
+        timezone //passing as it is, the name coincides
+    } })
 }
