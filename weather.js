fe5d7a2a99a3e0adb51b4cbe0fa19e0f9fc40a4b
Arnette Reggie <mixedworldcitizen@gmail.com>
2024-01-25 11:09:17 +0100

    changed to km/h, mm, chose weather api vars

---
+++ weather.js
@@ -0,0 +1,26 @@
+
+
+/*
+https://open-meteo.com/en/docs#api_form
+
+hourly vars:
+    temp,
+    apparent temp, 
+    precip(rain + showers + snow)
+    weather code (this is for icons)
+    wind speed(10m)
+
+daily vars:
+    weather code, 
+    max temp (2m), 
+    min temp (2m), 
+    max appar temp (2m), 
+    min appar temp (2m),
+    precip sum
+
+using current weather settings
+*/
+
+export function getWeather(latitude, longitude, timezone){
+
+}
