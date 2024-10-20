594e9e6b256de63ab073982986fd48eabdcfc680
linguawork <mixedworldcitizen@gmail.com>
2024-10-20 21:10:51 +0200

    24 10 20, js functions added, basic calculator is ready

---
+++ script.js
@@ -0,0 +1,21 @@
+const screen = document.getElementById("screen")
+
+function showOnScreen(input){
+    screen.value += input
+}
+
+function clearScreen(input){
+    screen.value = ""
+}
+
+function calculate(input){
+    try{
+        //eval function works as calculator
+        screen.value = eval(screen.value)
+    }
+    catch(error){
+        screen.value = 'error'
+    }
+        
+}  
+
